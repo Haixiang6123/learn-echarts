006403748734eb0fd5ba5178dfe126d0fea94bb5
Haixiang <haixiang6123@gmail.com>
2020-05-26 17:19:46 +0800

    添加 Vue

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>在 Vue 里使用 ECharts</h1>
+        <VueECharts :options="lineChartOption"/>
+    </div>
+</template>
+
+<script lang="ts">
+  import VueECharts from './VueECharts.vue'
+
+  export default {
+    data() {
+      return {
+        lineChartOption: {
+          title: {
+            text: '折线图'
+          },
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line'
+          }]
+        }
+      }
+    },
+    components: {
+      VueECharts
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ VueECharts.vue
@@ -0,0 +1,32 @@
+<template>
+    <div ref="container"></div>
+</template>
+
+<script>
+  import echarts from 'echarts'
+
+  export default {
+    props: ['options'],
+    data() {
+      return {
+        chart: null,
+      }
+    },
+
+    mounted() {
+      const {container} = this.$refs
+
+      const width = document.documentElement.clientWidth
+      container.style.width = width + 'px'
+      container.style.height = width * 1.2 + 'px'
+
+      this.chart = echarts.init(container, 'dark')
+
+      this.chart.setOption(this.options)
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>

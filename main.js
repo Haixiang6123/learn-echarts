f50c86eb39100a16723ea9f56681f9efd6b7c2e8
Haixiang <haixiang6123@gmail.com>
2020-05-26 16:30:50 +0800

    添加 loadMore

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ const lineChartOption = {
 
 
 // 指定图表的配置项和数据
+const xData = ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+const yData = [5, 20, 36, 10, 10, 20]
 const barCharOption = {
   title: {
     text: 'ECharts 入门示例'
@@ -36,36 +38,30 @@ const barCharOption = {
     data:['销量']
   },
   xAxis: {
-    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    data: xData
   },
   yAxis: {},
   series: [{
     name: '销量',
     type: 'bar',
-    data: [5, 20, 36, 10, 10, 20]
+    data: yData
   }]
 };
 
 // 使用刚指定的配置项和数据显示图表。
-myChart.setOption({
-  title: {
-    text: '折线图'
-  },
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [{
-    lineStyle: {
-       color: 'blue'
+myChart.setOption(barCharOption);
+
+loadMore.addEventListener('click', () => {
+  const key = '2020-01-07'
+  const value = 8
+  console.log('fuck')
+
+  myChart.setOption({
+    xAxis: {
+      data: [...xData, key]
     },
-    itemStyle: {
-      borderWidth: 10
-    },
-    data: [820, 932, 901, 934, 1290, 1330, 1320],
-    type: 'line'
-  }]
-});
+    series: [{
+      data: [...yData, value]
+    }]
+  })
+})

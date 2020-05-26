7612445af965dcc37fed82e5caeb3affb4f8ba48
Haixiang <haixiang6123@gmail.com>
2020-05-26 16:37:26 +0800

    添加加载更多

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const $chart = document.getElementById('main')
 
 // 基于准备好的dom，初始化echarts实例
 // 主题：dark, light, default
-const myChart = echarts.init($chart, 'light');
+const myChart = echarts.init($chart, 'light')
 
 const lineChartOption = {
   title: {
@@ -23,19 +23,19 @@ const lineChartOption = {
     data: [820, 932, 901, 934, 1290, 1330, 1320],
     type: 'line'
   }]
-};
+}
 
 
 // 指定图表的配置项和数据
-const xData = ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-const yData = [5, 20, 36, 10, 10, 20]
+let xData = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+let yData = [5, 20, 36, 10, 10, 20]
 const barCharOption = {
   title: {
     text: 'ECharts 入门示例'
   },
   tooltip: {},
   legend: {
-    data:['销量']
+    data: ['销量']
   },
   xAxis: {
     data: xData
@@ -46,22 +46,27 @@ const barCharOption = {
     type: 'bar',
     data: yData
   }]
-};
+}
 
 // 使用刚指定的配置项和数据显示图表。
-myChart.setOption(barCharOption);
+myChart.setOption(barCharOption)
+
+let i = 0
 
 loadMore.addEventListener('click', () => {
-  const key = '2020-01-07'
-  const value = 8
-  console.log('fuck')
+  const key = i
+  i += 1
+  const value = i + 100
+
+  xData = [...xData, key]
+  yData = [...yData, value]
 
   myChart.setOption({
     xAxis: {
-      data: [...xData, key]
+      data: xData
     },
     series: [{
-      data: [...yData, value]
+      data: yData
     }]
   })
 })
